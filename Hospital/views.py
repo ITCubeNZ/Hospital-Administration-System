e8e728cd2de6c6b5c2f5037513b189585fa21641
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-04-12 21:09:30 +1200

    Update views.py
    Added Index and Staff Views

--- Hospital/views.py
+++ Hospital/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
 
+def index(request):
+    """
+        Returns the HomePage
+    """
+    return render(request, "index.html")
+
+def staff(request):
+    """
+        Returns a list of Staff Members
+    """
+    return render(request, "staff.html")
