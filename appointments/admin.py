d8a28b0c794ec1e48ae93803fa16e279f3030a86
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-04-12 21:31:15 +1200

    Updated Admin
    Registered Models with Admin Panel

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Staff, Appointment, Patient
 
 # Register your models here.
+admin.site.register(Staff)
+admin.site.register(Patient)
+admin.site.register(Appointment)
