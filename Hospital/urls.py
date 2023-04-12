deb229cb83ecc432f1e4ce38c059a4e5e6b6d2d9
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-04-12 21:10:57 +1200

    Update urls.py
    Added urls for index and staff

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import index, staff
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='home'),
+    path('staff/', staff, name='staff')
 ]
