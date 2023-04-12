a057c2be1078f1599adbc698f22ac03bc3359ac8
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-04-13 10:04:01 +1200

    Changed Models
    Added Department Model, Fixed Staff, Patient, Appointment models.

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Staff, Appointment, Patient
+from .models import Staff, Appointment, Patient, Department
 
 # Register your models here.
 admin.site.register(Staff)
 admin.site.register(Patient)
 admin.site.register(Appointment)
+admin.site.register(Department)
--- appointments/models.py
+++ appointments/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Appointment(models.Model):
-    """
-        Model for Appointments 
-
-        Appointment ID: Primary Key
-        Patient ID: Foreign Key for Patient Table and should represent their NHI.
-        Staff ID: Foreign Key for Staff Table this should be representative of their current staff code.
-        Appointment Date: Should represent the date and time the appointment is booked for. 
-    """
-    appointment_id = models.AutoField(primary_key=True)
-    patient = models.ForeignKey(User, on_delete=models.CASCADE)
-    staff_id = models.CharField(max_length=20)
-    appointment_date = models.DateTimeField()
-    contact_phone = models.CharField(max_length=15)
-
-
 class Patient(models.Model):
     """
         Model for Patients
@@ -32,6 +16,10 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=50)
     dob = models.DateTimeField()
     phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        # String Representation
+        return "{}. {} {}".format(self.patient_id, self.first_name, self.last_name )
 
 class Staff(models.Model):
     """
@@ -50,5 +38,44 @@ class Staff(models.Model):
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=50)
     description = models.TextField()
+
+    def __str__(self):
+        # String representation
+        return "{} {}".format(self.first_name, self.last_name)
+
+class Appointment(models.Model):
+    """
+        Model for Appointments 
+
+        Appointment ID: Primary Key
+        Patient ID: Foreign Key for Patient Table and should represent their NHI.
+        Staff ID: Foreign Key for Staff Table this should be representative of their current staff code.
+        Appointment Date: Should represent the date and time the appointment is booked for. 
+    """
+    appointment_id = models.AutoField(primary_key=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField()
+    contact_phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return "Appointment for {} with {}".format(self.patient, self.staff_id)
+
+class Department(models.Model):
+    """
+        Models for hospital department. 
+
+        department_id = auto_id representing the ID
+        department_name = Represents the department_name
+        hod = Represents the head of department
+    """
+    department_id = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=50)
+    hod = models.ForeignKey(Staff, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # String Reperesentation
+        return "{}".format(self.department_name)
+
 
     
